598e88b8f098833743e2df4597a6b116857161ff
Dmitry Mishakov <welzevul0@gmail.com>
2022-12-28 06:55:37 +0300

    refactor: pom file and Car class for use in DB

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,5 +1,73 @@
 package web.model;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cars")
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String model;
+
+    private int mileage;
+
+    private String owner;
+
+    public Car() {
+    }
+
+    public Car(String model, int mileage, String owner) {
+        this.model = model;
+        this.mileage = mileage;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", mileage=" + mileage +
+                ", owner='" + owner + '\'' +
+                '}';
+    }
 }
